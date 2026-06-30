96c5a37cec9613c6f0c64bf3ffb6ad690093a2db
Ezequiel Demetras <ezequiel-d.s@hotmail.com>
2026-06-30 19:26:33 -0300

    develop subscriber

---
+++ Subscriber/Subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+const int port = 11000;
+using var udpClient = new UdpClient(port);
+
+while (true)
+{
+    var result = await udpClient.ReceiveAsync();
+    var message = Encoding.UTF8.GetString(result.Buffer);
+    Console.WriteLine(message);
+}
